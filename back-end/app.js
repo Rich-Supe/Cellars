e48f7cee441851b03419ae7e75e3092f0b3f2054
Rich-Supe <rsupe1993@aol.com>
2021-05-19 21:38:56 -0400

    added dependencies and created DB

---
+++ back-end/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(routes); // Connect all the routes
+
+// Security Middleware
+if (!isProduction) {
+	// enable cors only in development
+	app.use(cors());
+	}
+	// helmet helps set a variety of headers to better secure your app
+	app.use(helmet({
+	contentSecurityPolicy: false
+	}));
+
+	// Set the _csrf token and create req.csrfToken method
+	app.use(
+	csurf({
+			cookie: {
+			secure: isProduction,
+			sameSite: isProduction && "Lax",
+			httpOnly: true,
+			},
+	})
+	);
+
+
+	
+	module.exports = app;
