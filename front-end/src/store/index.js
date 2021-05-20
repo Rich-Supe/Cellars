b0b20d32479b926c7d596572e89af9ead360f37a
Rich-Supe <rsupe1993@aol.com>
2021-05-20 13:48:44 -0400

    added front-end

---
+++ front-end/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require('redux-logger').default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const rootReducer = combineReducers({
+
+});
