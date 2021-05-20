6981d632a42a670f2fa94094d60f52b73a985780
Rich-Supe <rsupe1993@aol.com>
2021-05-20 13:37:45 -0400

    >

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -4,13 +4,14 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
-
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
 
 module.exports = router;
+
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+//   });
+
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
