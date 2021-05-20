816cd011fab768fbebca7bf8b91571fee46d26e1
Rich-Supe <rsupe1993@aol.com>
2021-05-20 12:48:26 -0400

    added sessions

--- back-end/routes/api/session.js
+++ back-end/routes/api/session.js
@@ -40,4 +40,19 @@ router.post(
     }
   );
 
+  
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
 module.exports = router;
