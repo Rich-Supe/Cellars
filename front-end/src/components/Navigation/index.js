d88f6efd2655f9ab0cef93d445a19a13dcf5bcf1
Rich-Supe <rsupe1993@aol.com>
2021-05-20 20:57:25 -0400

    changed log in to a modal

--- front-end/src/components/Navigation/index.js
+++ front-end/src/components/Navigation/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -16,7 +17,7 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
+        <LoginFormModal />
         <NavLink to="/signup">Sign Up</NavLink>
       </>
     );
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ModalProvider } from "./context/Modal";
 
-import './index.css';
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import App from './App';
-
-import configureStore from './store';
-import * as sessionActions from './store/session';
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
-    restoreCSRF();
-  
-    window.csrfFetch = csrfFetch;
-    window.store = store;
-    window.sessionActions = sessionActions;
-  }
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
 
 function Root() {
-    return (
-      <Provider store={store}>
+  return (
+    <Provider store={store}>
+      <ModalProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </Provider>
-    );
-  }
+      </ModalProvider>
+    </Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -36,4 +38,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
