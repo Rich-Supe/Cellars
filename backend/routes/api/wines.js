3a78981e85234a375f7cc9f5ec95792a84a2e1de
Rich-Supe <rsupe1993@aol.com>
2021-05-23 21:29:01 -0400

    added API routes for cellar and wines

---
+++ backend/routes/api/wines.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { requireAuth } = require('../../utils/auth');
+const { User, Wine, Cellar, Review } = require('../../db/models');
+// const { Crate } = require('../../db/models');
+
+
+const router = express.Router();
+
+
+// Wine page routes
+
+// Grab all wines
+router.get(
+    '/',
+    // requireAuth,
+    asyncHandler(async(req, res) => {
+        const wines = await Wine.findAll();
+        //After implementing Crates, to access those wines, Crates may need another path
+        // let currUser = req.session.auth.userId;
+        // const winesInCrate = await Crate.findAll({where:{userId:currUser}});
+        // let crate = {}
+        // winesInCrate.forEach(wine => {
+        //     crate[wine.id]=wine.dataValues
+        // });
+        // res.render("wines", {wines, crate})
+        res.render("wines", {wines})
+    })
+
+)
+
+// Grab Wines by search  (need to fix-up)
+// router.get('/search/:searchCriteria/:selectedChoice', async (req, res, next) => {
+//     const searchCriteria = req.params.searchCriteria;
+//     const selectedChoice = req.params.selectedChoice;
+//     let results;
+    
+//     switch (searchCriteria) {
+//         case 'title':
+//             results = await Comic.findAll({where: {title: selectedChoice}});
+//             break;
+//         case 'author':
+//             results = await Comic.findAll({where: {author: selectedChoice}});
+//             break;
+//         case 'genre':
+//             results = await Comic.findAll({where: {genre: selectedChoice}});
+//             break;
+//         case 'keyword':
+//             results = await Comic.findAll({where: {title: { [Op.iLike]: `%${selectedChoice}%`}}});
+//             let results2 = await Comic.findAll({where: {author: { [Op.iLike]: `%${selectedChoice}%`}}});
+//             let results3 = await Comic.findAll({where: {genre: { [Op.iLike]: `%${selectedChoice}%`}}});
+//             results2.forEach(result => results.push(result));
+//             results3.forEach(result => results.push(result));
+//             break;
+//     }
+//     console.log(`*************************************${results}`)
+//     res.render('results', {results});
+//   });
+
+
+//Individual Wine route, including Reviews on the wine. 
+router.get(
+    '/:id(\\d+)',
+    asyncHandler(async(req, res) => {
+        const wineId = parseInt(req.params.id, 10);
+        const wine = await Wine.findByPk(wineId);
+        const reviews = await Review.findAll( {where:{ wineId }})
+        //After implementing Crates, to access those wines Crates may need another path
+        // let currUser = req.session.auth.userId;
+        // const winesInCrate = await Crate.findAll({where:{userId:currUser}});
+        // let crate = {}
+        // winesInCrate.forEach(wine => {
+        //     crate[wine.id]=wine.dataValues
+        // });
+        // res.render("wines", {wines, reviews, crate})
+        res.render("wines", {wine, reviews})
+    })
+)
+
+// More Review routes
+
+
+// Create reviews
+
+
+// Edit reviews
+
+
+// Delete reviews
+
+
+
+
+
+
+
+module.exports = router;
