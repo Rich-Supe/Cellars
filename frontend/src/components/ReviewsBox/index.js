a93abffc431e94238496634bd082251c24aafe5f
Rich-Supe <rsupe1993@aol.com>
2021-05-26 17:54:20 -0400

    fixing review carousel

--- frontend/src/components/ReviewsBox/index.js
+++ frontend/src/components/ReviewsBox/index.js
@@ -2,17 +2,10 @@
 import styles from './ReviewsBox.module.css'
 
 const ReviewsBox = ({props}) => {
-    // console.log(`PROPS:`, props)
     const review = props.review.review
-    // console.log(`Review prop on Reviews Component`, review)
-
-    // const createReview = (e) => {
-    //     e.preventDefault();
-        
-    // }
 
     return (
-        <div>{review}</div>
+        <div className={styles.reviewCard}>{review}</div>
     )
 }
 
