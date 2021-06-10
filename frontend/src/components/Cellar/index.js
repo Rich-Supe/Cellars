fafd74ad26d6cdd5c20afd66593c16fb8442dd73
Rich-Supe <rsupe1993@aol.com>
2021-06-10 14:11:41 -0400

    unsolved state issue

---
+++ frontend/src/components/Cellar/index.js
@@ -0,0 +1,31 @@
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import WineCard from '../WineCard'
+import {getCellar} from '../../store/users'
+import styles from './Cellar.module.css';
+
+const Cellar = ({id}) => {
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(getCellar(id));
+    }, [dispatch])
+    
+    const wines = useSelector((state) => Object.values(state))
+    console.log('Should be crate of wines:::', wines)
+    return (
+        <div className={styles.cellar}>
+            <p>CELLAR</p>
+            {/* <div className={styles.wineCardDiv}>
+                {wines?.map(wine => {
+                    return <div id={wine.id} key={wine.id} className={styles.WineCard}>
+                        <WineCard wine={wine} />
+                    </div>
+                })}
+            </div> */}
+        </div>
+    )
+}
+
+export default Cellar;
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,58 +1,61 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import styles from './Navigation.module.css';
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  
-  const openMenu = () => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const id = user.id
+
+    const openMenu = () => {
     if (showMenu) return;
     setShowMenu(true);
-  };
-  
-  useEffect(() => {
+    };
+
+    useEffect(() => {
     if (!showMenu) return;
 
     const closeMenu = () => {
-      setShowMenu(false);
+        setShowMenu(false);
     };
 
     document.addEventListener('click', closeMenu);
-  
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
 
-  const logout = (e) => {
+    return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
-  };
+    };
 
-  const profilePage = (e) => {
-      e.preventDefault();
-  }
+    const profilePage = (e) => {
+        e.preventDefault();
+    }
 
-  return (
-    <>
-        <button className={styles.profileBtn} onClick={openMenu}>
-        {/* <i className="fas fa-user-circle"></i> */}
-            {user.username}
-        </button>
-        {showMenu && (
-        <ul className="profile-dropdown">
-            {/* <li>{user.username}</li>
-            <li>{user.email}</li> */}
-            <li>
-                <button className={styles.dropdownBtn}>Your Profile</button>
-            </li>
-            <li>
-            <button className={styles.dropdownBtn} onClick={logout}>Log Out</button>
-            </li>
-        </ul>
-        )}
-    </>
-  );
+    return (
+        <>
+            <button className={styles.profileBtn} onClick={openMenu}>
+            {/* <i className="fas fa-user-circle"></i> */}
+                {user.username}
+            </button>
+            {showMenu && (
+            <ul className="profile-dropdown">
+                {/* <li>{user.username}</li>
+                <li>{user.email}</li> */}
+                <li>
+                    <button className={styles.dropdownBtn} onClick={()=> history.push(`/users/${id}`)}>Your Profile</button>
+                </li>
+                <li>
+                <button className={styles.dropdownBtn} onClick={logout}>Log Out</button>
+                </li>
+            </ul>
+            )}
+        </>
+    );
 }
 
 export default ProfileButton;
