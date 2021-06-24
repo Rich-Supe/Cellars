2ebf766b7751d5d964d969e071637adc94ae0269
Rich-Supe <rsupe1993@aol.com>
2021-06-24 16:47:03 -0400

    journal fix

--- frontend/src/components/Journal/JournalEntries.js
+++ frontend/src/components/Journal/JournalEntries.js
@@ -1,14 +1,20 @@
 
 
+// import { useHistory } from "react-router-dom";
+// import styles from './Journal.module.css'
 
-const JournalEntries = () => {
+// const JournalEntries = () => {
+//     const history = useHistory();
 
+//     return (
+//             <div className={styles.journal}>
+//                 <header>Welcome to your Journal!</header>
+//                 <div className={styles.journalDisplay}>
+//                     DEersplay
+//                     <button onClick={(e)=> history.push('/journal/edit')}>Edit</button>
+//                 </div>
+//             </div>
+//     )
+// }
 
-    return (
-        <>
-        ENTRIES
-        </>
-    )
-}
-
-export default JournalEntries;
+// export default JournalEntries;
